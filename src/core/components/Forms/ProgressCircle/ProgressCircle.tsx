799db51652425b136288b6c1237b70b510428fcd
jeremtab <taboada.jeremie@gmail.com>
2023-09-12 01:54:52 +0200

    feat(progress): handle caption

--- src/core/components/Forms/ProgressCircle/ProgressCircle.tsx
+++ src/core/components/Forms/ProgressCircle/ProgressCircle.tsx
@@ -2,15 +2,15 @@ import styled from '@emotion/styled';
 import {useMemo} from 'react';
 import {css, keyframes} from '@emotion/react';
 
-import {Box} from '../../Box/Box';
 import {themeProp, themeConst} from '../../../utils/types/theme';
-import type {BoxProps} from '../../Box/Box';
-import type {CSSColor, WithTheme} from '../../../utils/types/theme';
+import type {CSSColor, CSSWidth, WithTheme} from '../../../utils/types/theme';
 
 interface CircleProgressProps {
+    size: CSSWidth;
     bg?: WithTheme<CSSColor>;
     fg?: WithTheme<CSSColor>;
     value?: number;
+    caption?: boolean;
     thickness?: number;
 }
 
@@ -44,31 +44,52 @@ const Meter = styled(Track)<{fg?: WithTheme<CSSColor>; circumference: number; da
     transition: stroke-dashoffset 0.3s;
 `;
 
-export const ProgressCircle = ({value, bg, fg, thickness = 0.2, ...props}: CircleProgressProps & BoxProps) => {
+const Figure = styled.figure<{size: CSSWidth}>`
+    position: relative;
+    width: ${props => props.size};
+    height: ${props => props.size};
+    margin: 0;
+`;
+
+const Caption = styled.figcaption<{color?: WithTheme<CSSColor>}>`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font: ${themeConst('theme:fonts.ui')};
+    color: ${props => themeProp(props.color, themeConst('theme:colors.accent'))};
+    font-size: 0.9em;
+`;
+
+export const ProgressCircle = ({value, bg, fg, size, caption, thickness = 0.15}: CircleProgressProps) => {
     const baseSize = 512; // px
     const radius: number = baseSize / 2;
     const {circumference, dashoffset} = useMemo(() => {
         const circumference = radius * 2 * Math.PI;
         return {
             circumference,
-            dashoffset: circumference - ((value ?? 25) / 100) * circumference,
+            dashoffset: circumference - (value ?? 0.25) * circumference,
         };
     }, [radius, value]);
 
     return (
-        <Box
-            {...props}
-            el="figure"
+        <Figure
             role="meter"
+            size={size}
             aria-valuenow={value || 0}
-            aria-valuetext={value ? `${value}%` : 'Infinite Spinner'}
+            aria-valuetext={value ? `${value * 100}%` : 'Infinite Spinner'}
             aria-valuemin={0}
             aria-valuemax={100}
         >
+            {value !== undefined && caption && <Caption color={fg}>{(value * 100).toFixed(0)}%</Caption>}
             <Svg infinite={value === undefined} viewBox={`0 0 ${baseSize} ${baseSize}`}>
                 <Track bg={bg} thickness={thickness * baseSize} r={baseSize / 2} cx="50%" cy="50%" />
                 <Meter fg={fg} thickness={thickness * baseSize} r={baseSize / 2} cx="50%" cy="50%" circumference={circumference} dashoffset={dashoffset} />
             </Svg>
-        </Box>
+        </Figure>
     );
 };
--- src/examples/Progress.tsx
+++ src/examples/Progress.tsx
@@ -3,12 +3,12 @@ import {Button, ProgressCircle, Flex} from '../joy';
 
 export const Progress = () => {
     const [value, setValue] = useState(0);
-    const plusTen = useCallback(() => setValue(v => (v + 10) % 100), []);
+    const plusTen = useCallback(() => setValue(v => (v > 0.91 ? 0 : v + 0.1)), []);
     return (
         <Flex align="center" distribute="center" height="fill">
-            <ProgressCircle width="3rem" />
-            <ProgressCircle fg="theme:colors.success" width="3rem" />
-            <ProgressCircle width="3rem" value={value} />
+            <ProgressCircle size="3rem" />
+            <ProgressCircle fg="theme:colors.success" size="3rem" />
+            <ProgressCircle size="3rem" value={value} caption />
             <Button onClick={plusTen}>+10</Button>
         </Flex>
     );
