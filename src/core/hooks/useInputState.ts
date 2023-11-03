5af10c1d4f8ac9ba4a52a40beb6e6e930da7ba7a
Vincent Neel <20cent.neel@gmail.com>
2023-11-03 12:26:05 +0100

    feat(useInputState): improve useInputState to accept string in setValue

--- src/core/hooks/useInputState.ts
+++ src/core/hooks/useInputState.ts
@@ -5,14 +5,15 @@ type InputEventHandler =
     | FormEventHandler<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
     | ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>;
 
-type InputEvent =
+type InputEvent<T extends string> =
     | FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
-    | ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>;
+    | ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+    | T;
 
 export const useInputState = <T extends string>(initialValue: T) => {
     const [value, setValue] = useState<T>(initialValue);
-    const handleChange = useCallback<InputEventHandler>((event: InputEvent) => {
-        setValue(event.currentTarget.value as T);
+    const handleChange = useCallback<InputEventHandler>((event: InputEvent<T>) => {
+        setValue(typeof event === 'string' ? event : (event.currentTarget.value as T));
     }, []);
     return [value, handleChange] as const;
 };
