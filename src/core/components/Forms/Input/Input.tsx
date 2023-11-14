8bdd9c8ce282dbfb5b767db18f3b0c32cfa1361e
Vincent Neel <20cent.neel@gmail.com>
2023-11-14 16:46:52 +0100

    fix(input): fix Input adding every props to wrapper when type = search

--- src/core/components/Forms/Input/Input.tsx
+++ src/core/components/Forms/Input/Input.tsx
@@ -22,7 +22,7 @@ export const Input = ({style, className, type, ...props}: InputProps) => {
     );
     if (type === 'search') {
         return (
-            <InputWrapper className={className} {...props}>
+            <InputWrapper className={className}>
                 <svg viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="5.5" cy="5.5" r="4.5" stroke="black" strokeWidth="2" />
                     <rect width="6.98326" height="2.09498" rx="1.04749" transform="matrix(0.708801 0.705408 -0.708801 0.705408 9.01477 7.59613)" fill="black" />
