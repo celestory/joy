dcd7fc636cbb874dc90463fd7d8bf613b8e5d9fa
JonathanPicques <jonathan.picques@gmail.com>
2023-12-08 13:05:50 +0100

    chore: fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,18 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-    env: {browser: true, es2021: true},
-    extends: ['eslint:recommended', 'plugin:react-hooks/recommended', 'plugin:@typescript-eslint/recommended'],
-    plugins: ['react-refresh'],
-    //
+    env: {browser: true, es2020: true},
+    root: true,
     parser: '@typescript-eslint/parser',
-    parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+    plugins: ['react-refresh'],
+    extends: ['eslint:recommended', 'plugin:react-hooks/recommended', 'plugin:@typescript-eslint/recommended'],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
     //
     rules: {
         'prefer-template': 'error',
+        //
         'react-refresh/only-export-components': 'warn',
         //
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_', varsIgnorePattern: '^_', ignoreRestSiblings: true}],
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/consistent-type-imports': [
